95337b9292c99d2927014235a31ed1b50b3a675c
Rose Gao <roseg.gao@gmail.com>
2024-11-11 15:08:36 -0500

    Removing some tables

--- Student_Center_3.0_Database/Models/StudentCenterContext.cs
+++ Student_Center_3.0_Database/Models/StudentCenterContext.cs
@@ -11,8 +11,6 @@ namespace Student_Center_3._0_Database.Models
         public DbSet<User> Users { get; set; }
         public DbSet<Login> Logins { get; set; }
         public DbSet<Course> Courses { get; set; }  
-        public DbSet<CoursePrereq> CoursePrereqs { get; set; }
-        public DbSet<PrereqGroup> PrereqGroups { get; set; } 
         public DbSet<CoursePrerequisite> CoursePrerequisites { get; set; }
         public DbSet<StudentCourseHistory> StudentCourseHistories { get; set; }
 
@@ -38,28 +36,6 @@ namespace Student_Center_3._0_Database.Models
                 .HasForeignKey(sch => sch.userNum) // Define userNum as the foreign key
                 .OnDelete(DeleteBehavior.Cascade); // Specify cascade delete if a user is removed
 
-
-            // Initialize Composite Key for CoursePrereq
-            // necessary for groupId to be part of composite key?
-            modelBuilder.Entity<CoursePrereq>()
-                .HasKey(cp => new { cp.courseName, cp.prerequisite });
-
-
-            /*
-            // Initialize foreign key "course" in CoursePrereq + one : many relationship between courses : prerequisites
-            modelBuilder.Entity<CoursePrereq>()
-                .HasOne(cp => cp.Course)
-                .WithMany(c => c.CoursePrereqs)
-                .HasForeignKey(cp => cp.courseName)
-                .HasPrincipalKey(c => c.courseName)
-                .OnDelete(DeleteBehavior.Restrict); // Prevents cascade deletion if a course is removed
-            */
-
-            // Initialize foreign key "groupId" in CoursePrerq + one : many relationship between PrereqGroup entry : CoursePrereq entries
-            modelBuilder.Entity<CoursePrereq>()
-                .HasOne(cp => cp.PrereqGroup)
-                .WithMany(pg => pg.CoursePrereqs)
-                .HasForeignKey(cp => cp.groupId);
         }
     }
 }
