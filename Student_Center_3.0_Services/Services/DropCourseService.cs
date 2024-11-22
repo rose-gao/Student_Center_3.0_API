88164c99817d72d4e32a7a5a7f40b4317dc1a287
Rose Gao <roseg.gao@gmail.com>
2024-11-22 13:21:51 -0500

    DropCourse Complete

--- Student_Center_3.0_Services/Services/DropCourseService.cs
+++ Student_Center_3.0_Services/Services/DropCourseService.cs
@@ -1,4 +1,8 @@
-﻿namespace Student_Center_3._0_Services.Services
+﻿using Student_Center_3._0_Services.DTOs;
+using System.Text;
+using System.Text.Json;
+
+namespace Student_Center_3._0_Services.Services
 {
     public class DropCourseService
     {
@@ -16,15 +20,49 @@
                 throw new ArgumentException("UserNum and CourseNum must be greater than zero.");
             }
 
-            var courseResponse = await _httpClient.DeleteAsync($"api/StudentCourseEnrollment/{userNum}/{courseNum}");
+            // Delete course enrollment
+            var enrollmentResponse = await _httpClient.DeleteAsync($"api/StudentCourseEnrollment/{userNum}/{courseNum}");
+            if (!enrollmentResponse.IsSuccessStatusCode)
+            {
+                return $"{enrollmentResponse.StatusCode}";
+            }
 
+            // DECREMENT NUMBER OF ENROLLED STUDENTS IN THE COURSE
+            var courseResponse = await _httpClient.GetAsync($"api/Course/{courseNum}");
             if (!courseResponse.IsSuccessStatusCode)
             {
                 return $"{courseResponse.StatusCode}";
             }
 
-            return "OK";
+            var courseRecord = await courseResponse.Content.ReadFromJsonAsync<CourseDTO>();
+            if (courseRecord == null)
+            {
+                return "Course not found.";
+            }
+
+            // Ensure numEnrolled does not go below zero
+            if (courseRecord.numEnrolled > 0)
+            {
+                courseRecord.numEnrolled -= 1;
+            }
+            else
+            {
+                return "Cannot drop course; no students are currently enrolled.";
+            }
+
+            // Update course enrollment
+            var content = new StringContent(courseRecord.numEnrolled.ToString(), Encoding.UTF8, "application/json");
+            var updateCourseResponse = await _httpClient.PatchAsync($"api/Course/{courseNum}/update-enrollment", content);
+
+
+            if (!updateCourseResponse.IsSuccessStatusCode)
+            {
+                return $"{updateCourseResponse.StatusCode}";
+            }
+
+            return "Course successfully dropped.";
         }
+
     }
 
 }
